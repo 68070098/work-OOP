a4dd74fa4493ff2ad11021816d4e63d0e7d1737b
68070098 <68070098@kmitl.ac.th>
2026-02-27 01:55:55 +0700

    OOP_Lab10

--- Customer.java
+++ Customer.java
@@ -3,63 +3,91 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lab.oop.pkg01;
+import java.util.ArrayList;
 
 /**
  *
  * @author ADMIN
  */
 public class Customer {
+
     private String firstName;
     private String lastName;
-    private CheckingAccount acct;
+    private Account acct[];
+    private int numOfAccount;
 
     public Customer() {
-        this("", "", null);
+        acct = new Account[5];
+        numOfAccount = 0;
     }
 
     public Customer(String firstName, String lastName) {
-        this(firstName, lastName, null);
-    }
-
-    public Customer(String firstName, String lastName, CheckingAccount acct) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.acct = acct;
+        acct = new Account[5];
+        numOfAccount = 0;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public Account getAccount(int index) {
+        return acct[index];
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getLastName() {
         return lastName;
     }
 
-    public void setAcct(CheckingAccount acct) {
-        this.acct = acct;
-    }
-
-    public CheckingAccount getAcct() {
-        return acct;
-    }
-
+    @Override
     public String toString() {
-        if (acct == null) {
-            return firstName + " " + lastName + " doesn't have account.";
-        } else {
-            return "The " + firstName + " account has " + acct.getBalance() + " baht and " + acct.getCredit() + " credits.";
-        }
+        return firstName + " " + lastName + " has " + numOfAccount + " accounts.";
     }
 
-    public boolean equals(Customer c) {
-        return this.firstName.equals(c.firstName) && this.lastName.equals(c.lastName);
+    public void addAccount(Account acct) {
+        this.acct[numOfAccount] = acct;
+        numOfAccount++;
     }
+
+    public int getNumOfAccount() {
+        return numOfAccount;
+    }
+
 }
+
+
+//    private String firstName;
+//    private String lastName;
+//    private ArrayList<Account> acct;
+//
+//    public Customer2() {
+//        acct = new ArrayList<>();
+//    }
+//
+//    public Customer2(String firstName, String lastName) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        acct = new ArrayList<>();
+//    }
+//
+//    public void addAccount(Account acct) {
+//        this.acct.add(acct);
+//    }
+//
+//    public Account getAccount(int index) {
+//        return acct.get(index);
+//    }
+//
+//    public int getNumOfAccount() {
+//        return acct.size();
+//    }
+//
+//    public String getFirstName() { return firstName; }
+//    public String getLastName() { return lastName; }
+//
+//    @Override
+//    public String toString() {
+//        return firstName + " " + lastName + " has " + acct.size() + " accounts.";
+//    }
+//}
